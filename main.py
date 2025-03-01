2dc812d37191e8ffe74fe6ba8606edccef75c9c5
snowdevtester <snowdevtester@gmail.com>
2025-03-01 12:26:55 +0000

    modified response

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def sampleLOAStart():
       ]
     }
   """
-    return data
+    return jsonify(data)
 
 if __name__ == "__main__":
   app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
