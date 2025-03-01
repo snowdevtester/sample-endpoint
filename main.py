2908b498cf9d91e249f7a2687d8b7d1aff699bc7
snowdevtester <snowdevtester@gmail.com>
2025-03-01 12:34:58 +0000

    fix json structure

--- main.py
+++ main.py
@@ -47,7 +47,27 @@ def sampleLOAStart():
       ]
     }
   """
-    return jsonify(data)
+
+    data = {
+    "Report_Entry": [
+      {
+        "Manager_s_Network_ID": "incpra00",
+        "NTID": "inwicj01",
+        "Location_Code": "IND122",
+        "Employee_ID": "123XYZ",
+        "Estimated_Last_Day_of_Absence": "03/26/2025",
+        "Last_Day_of_Work": "02/18/2025",
+        "Emp_Name": "John Wick",
+        "First_Day_of_Absence": "02/19/2025",
+        "Manager_s_Email_address": "prasanth.c@ingrammicro.com",
+        "Reference_ID": "LEAVE_REQUEST_EVENT-6-15720",
+        "Region": "APAC",
+        "Country": "india",
+        "Associate_LOA_Days": "36"
+         }
+      ]
+    }
+    return data
 
 if __name__ == "__main__":
   app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
