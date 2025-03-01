19d79d9fa0165ff09d3c841481ef6091245189b8
snowdevtester <snowdevtester@gmail.com>
2025-03-01 17:14:17 +0000

    made variable response in sample LOA Start data

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import os
 
-from flask import Flask
-from flask import request
-from flask import jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -18,41 +16,22 @@ def postdata():
   if request.method == 'POST':
     print(request.content_type)
     print(request.json)
-    # print(request.form['eventType'])
-    return "Successfully received"
+    return { 
+      "Response" : "Successfully received",
+      "Data Sent" : request.json
+    }
 
 
 @app.route("/sampleloastart", methods = ['GET'])
 def sampleLOAStart():
   if request.method == 'GET':
-    # print(request.form['eventType'])
-    data = """
-    {
-      "Report_Entry":[
-        {
-          "Manager_s_Network_ID": "incpra00",
-          "NTID": "inwicj01",
-          "Location_Code": "IND122",
-          "Employee_ID": "123XYZ",
-          "Estimated_Last_Day_of_Absence": "03/26/2025",
-          "Last_Day_of_Work": "02/18/2025",
-          "Emp_Name": "John Wick",
-          "First_Day_of_Absence": "02/19/2025",
-          "Manager_s_Email_address": "prasanth.c@ingrammicro.com",
-          "Reference_ID": "LEAVE_REQUEST_EVENT-6-15720",
-          "Region": "APAC",
-          "Country": "india",
-          "Associate_LOA_Days": "36"
-        }
-      ]
-    }
-  """
-
-    data = {
-    "Report_Entry": [
-      {
+    report_entry = []
+    times = request.args.get('times')
+    for i in range(int(times)):
+      if(len(times) == 1):
+        report_entry.append({
         "Manager_s_Network_ID": "incpra00",
-        "NTID": "inwicj01",
+        "NTID": f"inwicj0{i}",
         "Location_Code": "IND122",
         "Employee_ID": "123XYZ",
         "Estimated_Last_Day_of_Absence": "03/26/2025",
@@ -64,8 +43,26 @@ def sampleLOAStart():
         "Region": "APAC",
         "Country": "india",
         "Associate_LOA_Days": "36"
-         }
-      ]
+         })
+      else:
+        report_entry.append({
+        "Manager_s_Network_ID": "incpra00",
+        "NTID": f"inwicj{i}",
+        "Location_Code": "IND122",
+        "Employee_ID": "123XYZ",
+        "Estimated_Last_Day_of_Absence": "03/26/2025",
+        "Last_Day_of_Work": "02/18/2025",
+        "Emp_Name": "John Wick",
+        "First_Day_of_Absence": "02/19/2025",
+        "Manager_s_Email_address": "prasanth.c@ingrammicro.com",
+        "Reference_ID": "LEAVE_REQUEST_EVENT-6-15720",
+        "Region": "APAC",
+        "Country": "india",
+        "Associate_LOA_Days": "36"
+         })
+    
+    data = {
+    "Report_Entry": report_entry
     }
     return data
 
